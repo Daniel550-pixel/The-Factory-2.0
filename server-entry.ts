9b81bd88c3a462577149ec0be68bcc43060917fa
Daniel550-pixel <513785@vistacollege.nl>
2026-09-06 14:42:57 +0200

    Load environment variables before Factory server startup

--- server-entry.ts
+++ server-entry.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import { registerKnowledgeRoutes } from './server/knowledge-routes';
 import { registerKnowledgeAdapterRoutes } from './server/knowledge-adapter-routes';
