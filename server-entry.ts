3df7d321953a34c8c32d24ffbcc2b7a89a57c8db
Daniel550-pixel <513785@vistacollege.nl>
2026-09-06 14:28:34 +0200

    Register Ox Alpha knowledge route

--- server-entry.ts
+++ server-entry.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { registerKnowledgeRoutes } from './server/knowledge-routes';
 import { registerKnowledgeAdapterRoutes } from './server/knowledge-adapter-routes';
+import { registerOxAlphaRoutes } from './server/ox-alpha-routes';
 
 const originalAll = express.application.all;
 let knowledgeRoutesRegistered = false;
@@ -9,6 +10,7 @@ express.application.all = function patchedAll(path: string, ...handlers: any[]) 
   if (path === '/api/*' && !knowledgeRoutesRegistered) {
     registerKnowledgeRoutes(this);
     registerKnowledgeAdapterRoutes(this);
+    registerOxAlphaRoutes(this);
     knowledgeRoutesRegistered = true;
   }
   return originalAll.call(this, path, ...handlers);
